73f095d1ab3bad880f435676c0472e30bba276f9
bikashx <chakukhala1@gmail.com>
2015-08-09 14:17:59 -0400

    The first upload to git

---
+++ beginningjava.java
@@ -0,0 +1,13 @@
+// Begining first java program after long time
+
+
+class sum
+{
+	public static void main(String[] args)      // declaring main method
+	{
+	int i=1;
+	int j=2;
+	int k=i+j;
+	System.out.println("this is the value of two addition": k)
+	}
+}
