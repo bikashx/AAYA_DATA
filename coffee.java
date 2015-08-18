5d35a13f834e547233ac79869277bb88cfb592c0
Bikash <chakukhala1@gmail.com>
2015-08-17 21:02:53 -0400

    commit

---
+++ coffee.java
@@ -0,0 +1,41 @@
+// Making a Coffee
+// Bikash Awale
+//Date:
+
+public class Coffee 
+{
+
+public void Ingredient()
+{
+Cream
+Sugar
+Coffee
+}
+
+public void Size()
+{
+Small
+Medium
+Large
+}
+
+Public void Boil()
+{
+Water
+Heat
+}
+
+public void Flavor()
+{
+Milk
+Bold
+Mocha
+}
+
+Public void main(String [] args)
+{
+Make = Ingredient.Cream +Flavor.Milk + Size.Large + Boil.water
+
+Coff= Make; 
+}
+}
